c066bf58b609be712e05df44a016ce5c81a7a0df
Robert <rhead82@hotmail.com>
2021-06-29 21:12:47 -0500

    reducers and redux polish

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,37 @@
-import express from 'express'
-import asyncHandler from 'express-async-handler'
-const router = express.Router()
-import Product from '../models/productModel.js'
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/productModel.js';
 
 // @desc Fetch all products
 // @route Get /api/products
 // @access Public
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc Fetch single product
 // @route Get /api/products/:id
 // @access Public
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
 
     if (product) {
-        res.json(product)
+      res.json(product);
     } else {
-        res.status(404)
-        throw new Error('Product not found')
+      res.status(404);
+      throw new Error('Product not found');
     }
-    res.json(product)
-}))
+    res.json(product);
+  })
+);
 
-export default router
+export default router;
